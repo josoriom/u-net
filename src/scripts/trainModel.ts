6503705f2e83b05c1e62bfec8bc641dcd5b09aa5
josoriom <josoriom93@gmail.com>
2025-01-07 08:06:21 +0800

    feat add earlyStopping

--- src/scripts/trainModel.ts
+++ src/scripts/trainModel.ts
@@ -1,4 +1,4 @@
-import { LayersModel, metrics, train } from '@tensorflow/tfjs-node';
+import { LayersModel, metrics, train, callbacks } from '@tensorflow/tfjs-node';
 
 import { diceCoefficient } from '../metrics/diceCoefficient.js';
 import { TrainingData } from '../types/TrainData.js';
@@ -11,12 +11,12 @@ export async function trainModel(
     learningRate?: number;
   } = {},
 ) {
-  const { epochs = 50, learningRate = 0.001 } = options;
+  const { epochs = 50, learningRate = 0.0001 } = options;
   const { training, validation, batchSize } = data;
   model.compile({
     optimizer: train.adam(learningRate),
     loss: metrics.binaryCrossentropy,
-    metrics: [diceCoefficient],
+    metrics: ['accuracy'],
   });
 
   await model.fit(training.x, training.y, {
@@ -24,5 +24,13 @@ export async function trainModel(
     shuffle: false,
     batchSize,
     epochs,
+    callbacks: [
+      callbacks.earlyStopping({
+        monitor: 'val_loss',
+        patience: 5,
+        mode: 'min',
+        verbose: 1,
+      }),
+    ],
   });
 }
